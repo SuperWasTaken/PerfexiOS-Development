7e47a3aae43dc6f6eb377b09a80a87fd106d6054
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2024-03-17 17:32:24 +0100

    guiiii

---
+++ PerfexiOS/Desktop/Extensions/CanvasFix.cs
@@ -0,0 +1,32 @@
+﻿using Cosmos.System.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfexiOS.Desktop.Extensions
+{
+    public static class CanvasFix
+    {
+
+        public static void DrawImageA(this Canvas canv,Image img,int x,int y)
+        {
+
+            for (int X = 0; X < img.Width; X++)
+            {
+                for (int Y = 0; Y < img.Height; Y++)
+                {
+                    var c = Color.FromArgb(img.RawData[X + Y * img.Width]);
+                    if (c.A > 0)
+                    {
+                        canv.DrawPoint(c, X + x, Y + y);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
